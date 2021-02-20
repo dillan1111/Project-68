761417f720bc5804fb7b922686b6bef23b719d52
dillan1111 <70030386+dillan1111@users.noreply.github.com>
2021-02-20 14:20:15 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Header } from 'react-native-elements';
+import { createAppContainer} from 'react-navigation'; 
+import { createBottomTabNavigator} from 'react-navigation-tabs'; 
 
-export default class App extends React.Component {
+
+import fb from './screens/fb';
+import insta from './screens/in';
+
+
+
+export default class App extends Component {
   render() {
     return (
-      <View>
-        <AppContainer />
-      </View>
-    );
+    <View>
+      <Appcontainer />
+      <Header backgroundColor={'#9A4DFE'} 
+        centerComponent={{text: 'Buzz App', style:{color: '000000', fontSize:20, fontWeight: 1000}}}/>
+    </View>
+        
+    )
   }
 }
 
-
-
-var AppNavigator = createSwitchNavigator({
-  HomeScreen: HomeScreen,
-});
-
-const AppContainer = createAppContainer(AppNavigator)
+var TabNavigator = createBottomTabNavigator({
+  Facebook:{screen: fb},
+  Instagram:{screen: insta}
+})
+const Appcontainer = createAppContainer(TabNavigator)
